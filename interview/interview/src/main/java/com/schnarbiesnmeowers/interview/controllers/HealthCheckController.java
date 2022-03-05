98f8cc5f4f67929c3573ac83e58b75c68e39929b
schnarbiemeows <dylnksslr53@gmail.com>
2022-03-05 12:43:42 -0500

    removing REST healthcheck

--- interview/interview/src/main/java/com/schnarbiesnmeowers/interview/controllers/HealthCheckController.java
+++ interview/interview/src/main/java/com/schnarbiesnmeowers/interview/controllers/HealthCheckController.java
@@ -2,23 +2,23 @@ package com.schnarbiesnmeowers.interview.controllers;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(path="/healthcheck")
+//@RestController
+//@RequestMapping(path="/healthcheck")
 public class HealthCheckController {
 
 	private static final Logger applicationLogger = LogManager.getLogger("FileAppender");
 	
-	@GetMapping(path="/ping")
+	//@GetMapping(path="/ping")
 	public String healthcheck() {
 		logAction("success");
 		return "pung!";
 	}
 	
-	@GetMapping(path="/pong")
+	//@GetMapping(path="/pong")
 	public String healthcheck2() {
 		logAction("success");
 		return "pong!";
